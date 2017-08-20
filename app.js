507c79fb22a2fc438b5ca9709585585a19fa1f92
madhubabuanchuri <madhuanchuri@outlook.com>
2017-08-19 21:59:13 -0400

    changed to pratik

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ myApp.factory("bookService", function() {
 
 myApp.controller("HeaderCtrl", function($scope, $location) {
 	$scope.appDetails = {};
-	$scope.appDetails.title = "Madhu Babu Anchuri";
+	$scope.appDetails.title = "Pratik Nellutla";
 	$scope.appDetails.tagline = "Software Developer";
 	
 	$scope.nav = {};
